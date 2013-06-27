e7391097c0fc5fbadac85856b14e1981d2ae5dbd
acaly <wuzhenwei35@gmail.com>
2013-06-27 12:11:01 +0800

    Mark for update fixed render bug

--- core/block/tile/TileEntityScreen.java
+++ core/block/tile/TileEntityScreen.java
@@ -4,4 +4,9 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityScreen extends TileEntity {
 
+	public void updateEntity() {
+		if (worldObj.isRemote) {
+			worldObj.markBlockForRenderUpdate(xCoord, yCoord, zCoord);
+		}
+	}
 }
